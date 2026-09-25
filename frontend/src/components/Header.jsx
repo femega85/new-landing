54eb89a9fe8a4a18d9db6405c43d2948ec30bdba
femega85 <femega85@gmail.com>
2026-09-25 15:15:16 -0500

    Remove header horizontal margins

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -32,7 +32,7 @@ const Header = () => {
           : 'bg-transparent'
       }`}
     >
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="w-full px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-20">
           {/* Logo */}
           <div
